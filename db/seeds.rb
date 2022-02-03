82a2a687d06cbf698a092b15dc4e7fc7b4dfab45
Laurie Roy <laurieroy.dev@gmail.com>
2022-02-02 16:01:52 -0800

    Update seed file with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 general = Channel.create(name: "#general")
- random = Channel.create(name: "#random")
- best_kills = Channel.create(name: "#best-kills")
+random = Channel.create(name: "#random")
+best_kills = Channel.create(name: "#best-kills")
 
- general.messages.create(message: "This is for relevant information only! Memes, murders, etc. should be kept to other channels!", username: "Chucky")
- general.messages.create(message: "Lol first post!", username: "Freddy")
- general.messages.create(message: "*Chucky has unfriended Freddy*", username: "Chucky")
+puts "3 channels created"
 
- random.messages.create(message: "Gonna start going after coding school students. Any tips?", username: "Freddy")
- random.messages.create(message: "They're all going to be dreaming about coding, is it really worth it?", username: "Chucky")
- random.messages.create(message: "Lol Freddy vs JSON", username: "Jason")
- random.messages.create(message: "They understand that suffering is a form of pleasure.", username: "Hellraiser")
+chucky = User.create(username: "Chucky", password: "guest")
+freddy = User.create(username: "Freddy", password: "guest")
+hellraiser = User.create(username: "Hellraiser", password: "guest")
+jason = User.create(username: "Jason", password: "guest")
+ghostface = User.create(username: "Ghostface", password: "guest")
+leatherface = User.create(username: "ChainsawManAggieFan", password: "guest")
+puts "6 users created"
 
- best_kills.messages.create(message: "Two words: Garage Door.", username: "Ghostface")
- best_kills.messages.create(message: "Hey Freddy, heard your students wanted to learn about hooks.", username: "Hellraiser")
- best_kills.messages.create(message: "Lol heard", username: "ChainsawManAggieFan")
+general.messages.create(message: "This is for relevant information only! Memes, murders, etc. should be kept to other channels!", username: "Chucky")
+general.messages.create(message: "Lol first post!", username: "Freddy")
+general.messages.create(message: "*Chucky has unfriended Freddy*", username: "Chucky")
+
+random.messages.create(message: "Gonna start going after coding school students. Any tips?", username: "Freddy")
+random.messages.create(message: "They're all going to be dreaming about coding, is it really worth it?", username: "Chucky")
+random.messages.create(message: "Lol Freddy vs JSON", username: "Jason")
+random.messages.create(message: "They understand that suffering is a form of pleasure.", username: "Hellraiser")
+
+best_kills.messages.create(message: "Two words: Garage Door.", username: "Ghostface")
+best_kills.messages.create(message: "Hey Freddy, heard your students wanted to learn about hooks.", username: "Hellraiser")
+best_kills.messages.create(message: "Lol heard", username: "ChainsawManAggieFan")
+puts "Messages created: 3 best kills, 3 general and 4 randos"
